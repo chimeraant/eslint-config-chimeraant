6c7e1559ecfa0f8d67d347618982f5d6798c5f66
aabccd021 <aabccd021@gmail.com>
2023-01-17 16:15:53 +0700

    fix: new

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ module.exports = {
     'simple-import-sort',
     'unused-imports',
   ],
-  ignorePatterns: ['**/*.js', 'dist/', 'node_modules'],
+  ignorePatterns: ['node_modules'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['**/tsconfig.**'],
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@typescript-eslint/strict',
     'plugin:functional/all',
     'plugin:fp-ts/all',
@@ -22,8 +24,24 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/default-param-last': 'error',
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/no-array-constructor': 'error',
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    '@typescript-eslint/no-loop-func': 'error',
+    '@typescript-eslint/no-redundant-type-constituents': 'error',
     '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unnecessary-qualifier': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-useless-empty-export': 'error',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/sort-type-constituents': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'error',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
     '@typescript-eslint/unbound-method': 'off',
+    'fp-ts/no-module-imports': ['error', { allowTypes: true }],
     'functional/functional-parameters': 'off',
     'functional/no-mixed-type': 'off',
     'prettier/prettier': ['error', { singleQuote: true, printWidth: 100 }],
@@ -31,7 +49,7 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     curly: ['error', 'all'],
     eqeqeq: 'error',
-    'max-len': ['error', { code: 100 }],
+    'max-len': ['error', { code: 100, ignoreUrls: true }],
     'no-else-return': 'error',
     'no-undef-init': 'error',
     'no-unsafe-optional-chaining': 'error',
@@ -44,19 +62,11 @@ module.exports = {
     'prefer-template': 'error',
 
     // https://github.com/sweepline/eslint-plugin-unused-imports#usage
-    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
     'unused-imports/no-unused-imports-ts': 'error',
     'unused-imports/no-unused-vars': [
       'error',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
   },
-  overrides: [
-    {
-      files: 'src/bin.ts',
-      rules: {
-        'functional/no-expression-statement': ['error', { ignorePattern: '^main\\(\\);$' }],
-      },
-    },
-  ],
 };
